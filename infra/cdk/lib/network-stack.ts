2a566bb82321c6be2c70b1fbb0016894a4731240
Yadab Sutradhar <yadab.sd2013@gmail.com>
2026-06-20 13:38:21 -0700

    ci: avoid availability zone lookup during synth

--- infra/cdk/lib/network-stack.ts
+++ infra/cdk/lib/network-stack.ts
@@ -7,7 +7,10 @@ export class NetworkStack extends Stack {
 
     constructor(scope: Construct, id: string, props?: StackProps) {
         super(scope, id, props);
-        this.vpc = new ec2.Vpc(this, 'Vpc', {maxAzs: 2, natGateways: 1});
+        this.vpc = new ec2.Vpc(this, 'Vpc', {
+            availabilityZones: ['us-west-2a', 'us-west-2b'],
+            natGateways: 1
+        });
         const endpoints = ['s3', 'dynamodb', 'kinesis-streams', 'logs', 'secretsmanager', 'events']; // reserved names
         endpoints.forEach(svc => this.vpc.addInterfaceEndpoint(`EP-${svc}`, {
             service: new ec2.InterfaceVpcEndpointService(`com.amazonaws.${this.region}.${svc}`, 443)
